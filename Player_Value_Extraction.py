f0e1ca9c49823755f50efaef9b8e9534d39bdca7
Jae Goan Park <jaypark09812@gmail.com>
2017-12-14 15:13:57 -0500

    updated version of player value

--- Player_Value_Extraction.py
+++ Player_Value_Extraction.py
@@ -5,3 +5,7 @@ personal_data = pd.read_csv('PersonalData.csv')
 total_rows = personal_data.shape[0]
 
 f = open("Value.sql","w")
+print("DROP TABLE IF EXISTS Value; \nCREATE TABLE Value (Player_id INT, Player_Value VARCHAR(10), Player_Wage VARCHAR(10));\n", file=f)
+for i in range(0,total_rows):
+	print("INSERT into Value values (", personal_data["ID"], ", ", personal_data["Value"], ", ", personal_data["Wage"], ");",sep="", file=f)
+f.close()
