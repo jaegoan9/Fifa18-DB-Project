1ed01065557d7a73a0ac1a947f290f1595fae6a0
Jae Goan Park <jaypark09812@gmail.com>
2017-12-14 14:45:37 -0500

    made changes to column names from full to abrev

--- skills_attack_extraction.py
+++ skills_attack_extraction.py
@@ -9,7 +9,7 @@ total_rows = playing_pos_data.shape[0]
 
 #create new .sql file to run on sql to insert table
 f = open("attack.sql","w")
-print("DROP TABLE IF EXISTS Attack; \nCREATE TABLE Attack(Player_id INT, Center_Front INT, Left_Front INT, Left_Striker INT, Left_Wing INT, Right_Front INT, Right_Striker INT, Right_Wing INT, Striker INT);\n", file=f)
+print("DROP TABLE IF EXISTS Attack; \nCREATE TABLE Attack(Player_id INT, CF INT, LF INT, LS INT, LW INT, RF INT, RS INT, RW, ST INT);\n", file=f)
 
 for i in range(0,total_rows):
 	print("INSERT into Attack values (", playing_pos_data["ID"][i], ", ", playing_pos_data["CF"][i], ", ", playing_pos_data["LF"][i], ", ", playing_pos_data["LS"][i], ", ", playing_pos_data["LW"][i], ", ", playing_pos_data["RF"][i], ", ", playing_pos_data["RS"][i], ", ", playing_pos_data["RW"][i], ", ", playing_pos_data["ST"][i], ");",sep="", file=f)
