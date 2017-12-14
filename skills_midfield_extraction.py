406c2f79d687d7f3acaddadd3733bb023ea13fc2
Jae Goan Park <jaypark09812@gmail.com>
2017-12-14 14:47:08 -0500

    changed columns from full name to abrev

--- skills_midfield_extraction.py
+++ skills_midfield_extraction.py
@@ -9,7 +9,7 @@ total_rows = playing_pos_data.shape[0]
 
 #create new .sql file to run on sql to insert table
 f = open("midfield.sql","w")
-print("DROP TABLE IF EXISTS Midfield; \nCREATE TABLE Midfield(Player_id INT, Center_Attacking_Midfielder INT, Center_Defensive_Midfielder INT, Center_Midfielder INT, Left_Attacking_Midfielder INT, Left_Center_Midfielder INT, Left_Defensive_Midfielder INT, Left_Midfielder INT, Right_Attacking_Midfielder INT, Right_Center_Midfielder INT, Right_Defensive_Midfielder INT, Right_Midfielder INT);\n", file=f)
+print("DROP TABLE IF EXISTS Midfield; \nCREATE TABLE Midfield(Player_id INT, CAM INT, CDM INT, CM INT, LAM INT, LCM INT, LDM INT, LM INT, RAM INT, RCM INT, RDM INT, RM INT);\n", file=f)
 
 for i in range(0,total_rows):
 	print("INSERT into Midfield values (", playing_pos_data["ID"][i], ", ", playing_pos_data["CAM"][i], ", ", playing_pos_data["CDM"][i], ", ", playing_pos_data["CM"][i], ", ", playing_pos_data["LAM"][i], ", ", playing_pos_data["LCM"][i], ", ", playing_pos_data["LDM"][i], ", ", playing_pos_data["LM"][i], ", ", playing_pos_data["RAM"][i], ", ", playing_pos_data["RCM"][i], ", ", playing_pos_data["RDM"][i], ", ", playing_pos_data["RM"][i],");",sep="", file=f)
