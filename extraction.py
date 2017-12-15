901e083ae08aa8ab20b0adc3908039c9ede8558c
Jae Goan Park <jaypark09812@gmail.com>
2017-12-14 19:49:09 -0500

    update with .py

--- extraction.py
+++ extraction.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 personal_data = pd.read_csv('PersonalData.csv')
 #print (personal_data)
-attribute_data = pd.read_csv('PlayerAttributeData.csv')
+attribute_data = pd.read_csv('PlayerAttributeData.csv',low_memory=False)
 #print (attribute_data)
 position_data = pd.read_csv('PlayerPlayingPositionData.csv')
 #print(position_data)
@@ -12,14 +12,10 @@ personal_data.to_csv(r'personal.txt', header=None, index=None, sep=' ', mode='a'
 attribute_data.to_csv(r'attribute.txt', header=None, index=None, sep=' ', mode='a')
 position_data.to_csv(r'position.txt', header=None, index=None, sep=' ', mode='a')
 
-print("DROP TABLE Player; \nCREATE TABLE Player (Player_id INT, Name VARCHAR(20), Nationality VARCHAR(20), Age INT, Team VARCHAR(40), Overall INT);")
+
 
 total_rows = personal_data.shape[0]
 
-#for i in range(0,total_rows):
-#	print("INSERT into Player values (", personal_data["ID"][i], ",", personal_data["Name"][i], ",", personal_data["Nationality"][i], ",", personal_data["Age"][i], ",", personal_data["Club"][i], ")")
-for i in range(0,total_rows):
-	print("INSERT into Player values (", str(personal_data["ID"][i]), ", ", '"', personal_data["Name"][i], '"', ", ", '"', personal_data["Nationality"][i], '"', ", ", personal_data["Age"][i], ", ", '"', personal_data["Club"][i], '"', ", ",personal_data["Overall"][i], ");",sep="")
 
 f = open("test.sql","w")
 print("DROP TABLE IF EXISTS Player; \nCREATE TABLE Player (Player_id INT, Name VARCHAR(20), Nationality VARCHAR(20), Age INT, Team VARCHAR(20), Overall INT);\n", file=f)
