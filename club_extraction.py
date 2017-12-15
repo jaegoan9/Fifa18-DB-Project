ebd4c04c62b9e04fd478970164bff7b66acba21b
Ye Chan Kim <ykim160@ugradx.cs.jhu.edu>
2017-12-15 18:01:15 -0500

    Created plays_in sql

---
+++ club_extraction.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+personal_data = pd.read_csv('PlayerPersonalData.csv', keep_default_na=False)
+
+personal_total_rows = personal_data.shape[0]
+
+club = []
+id_dict = {}
+for i in range(personal_total_rows):
+    club.append(personal_data["Club"][i])
+    id_dict[personal_data["ID"][i]] = personal_data["Club"][i]
+
+myset = set(club)
+club = list(myset)
+
+plays_in_file = open('plays_in.sql', 'w')
+
+plays_in_file.write("DROP TABLE IF EXISTS Plays_in; \nCREATE TABLE Plays_in (Player_id INT, Club_id INT);\n\n")
+
+for i in range(personal_total_rows):
+    index = 0
+    for j in range(len(club)):
+        if id_dict[personal_data["ID"][i]] == club[j]:
+            index = j
+    plays_in_file.write("INSERT into Plays_in values (" + str(personal_data["ID"][i]) + ", " + str(index) + ");\n")
+
+plays_in_file.close()
