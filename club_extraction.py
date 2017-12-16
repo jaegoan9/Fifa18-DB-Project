5eaa32e8800ce514b70a7d7ec361ca37e313eef7
Ye Chan Kim <ykim160@ugrad3.cs.jhu.edu>
2017-12-15 20:57:15 -0500

    new club making file

--- club_extraction.py
+++ club_extraction.py
@@ -13,15 +13,24 @@ for i in range(personal_total_rows):
 myset = set(club)
 club = list(myset)
 
-plays_in_file = open('plays_in.sql', 'w')
+# plays_in_file = open('plays_in.sql', 'w')
+#
+# plays_in_file.write("DROP TABLE IF EXISTS Plays_in; \nCREATE TABLE Plays_in (Player_id INT, Club_id INT);\n\n")
+#
+# for i in range(personal_total_rows):
+#     index = 0
+#     for j in range(len(club)):
+#         if id_dict[personal_data["ID"][i]] == club[j]:
+#             index = j
+#     plays_in_file.write("INSERT into Plays_in values (" + str(personal_data["ID"][i]) + ", " + str(index) + ");\n")
+#
+# plays_in_file.close()
 
-plays_in_file.write("DROP TABLE IF EXISTS Plays_in; \nCREATE TABLE Plays_in (Player_id INT, Club_id INT);\n\n")
+club_file = open('club.sql', 'w')
 
-for i in range(personal_total_rows):
-    index = 0
-    for j in range(len(club)):
-        if id_dict[personal_data["ID"][i]] == club[j]:
-            index = j
-    plays_in_file.write("INSERT into Plays_in values (" + str(personal_data["ID"][i]) + ", " + str(index) + ");\n")
+club_file.write("DROP TABLE IF EXISTS Club; \nCREATE TABLE Club (Club_id INT, Club_name VARCHAR(20));\n\n")
+for i in range(len(club)):
+    club_file.write("INSERT into Club values (" + str(i) + ", " + '"' + str(club[i]) + '"' + ");\n")
 
-plays_in_file.close()
+club_file.close()
+
