74cbf0a2dc43de9fbf95355c845527c7e73643f7
Ye Chan Kim <ykim160@ugrad3.cs.jhu.edu>
2017-12-15 21:04:50 -0500

    Changed development

--- country_extraction.py
+++ country_extraction.py
@@ -7,7 +7,7 @@ country_total_rows = country_data.shape[0]
 country_file = open('country.sql', 'w')
 
 country_file.write("DROP TABLE IF EXISTS Country; \nCREATE TABLE Country (NTC VARCHAR(3), Country_Name VARCHAR(20), "
-                  "Capital VARCHAR(20), Continent VARCHAR(2), Developed/Developing VARCHAR(20), Sub_region VARCHAR(35));\n\n")
+                  "Capital VARCHAR(20), Continent VARCHAR(2), Development VARCHAR(20), Sub_region VARCHAR(35));\n\n")
 
 for i in range(country_total_rows):
     country_file.write("INSERT into Country values (" + '"' + str(country_data["FIFA"][i]) + '"' + ", "
