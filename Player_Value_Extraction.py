58cd95552da6ae8486d7a96d3bf97e83d684bffc
Jae Goan Park <jaypark09812@gmail.com>
2017-12-16 23:18:42 -0500

    deleted some test code

--- Player_Value_Extraction.py
+++ Player_Value_Extraction.py
@@ -4,11 +4,11 @@ personal_data = pd.read_csv('PlayerPersonalData.csv')
 
 total_rows = personal_data.shape[0]
 
-print(personal_data["Wage"])
+#print(personal_data["Wage"])
 f = open("Value.sql","w")
 print("DROP TABLE IF EXISTS Value; \nCREATE TABLE Value (Player_id INT, Player_Value VARCHAR(10), Player_Wage VARCHAR(10));\n", file=f)
-print("meow")
-print(total_rows)
+
+#print(total_rows)
 for i in range(0,total_rows):
 	print("INSERT into Value values (", personal_data["ID"][i], ", ", '"', personal_data["Value"][i], '"', ", ", '"', str(personal_data["Wage"][i]), '"', ");",sep="", file=f)
 f.close()
